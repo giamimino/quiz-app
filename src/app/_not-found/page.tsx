5f6a463826b771a9f5b274d911016a0913e7a5b8
Gia <miminoshvili2102@example.com>
2025-06-08 18:59:19 +0400

    everything reseted vercel bugs we will fix later

---
+++ src/app/_not-found/page.tsx
@@ -0,0 +1,10 @@
+export default function NotFoundPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-2">404 - Page Not Found</h1>
+        <p className="text-lg text-gray-600">Oops! That page doesn&#39;t exist.</p>
+      </div>
+    </main>
+  );
+}
